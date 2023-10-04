5bfcf28741e048956a4b81573cf0ff7f51240823
greegouze <gregory.silvestri45@gmail.com>
2023-10-04 17:20:41 +0200

    init

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import router from './router';
 
 import './main.css';
+import { RouterProvider } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
