5910bd1a6b443ac9274807fccffdac110e40b78b
Justine Clément-Savary <clement.savary.justine@gmail.com>
2023-10-05 12:12:13 +0200

    Loader ok

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import Loader from '../components/Loader';
+
+function Home() {
+  return (
+    <>
+      <Loader />
+      <p>Hello</p>
+    </>
+  );
+}
+
+export default Home;
--- src/router.jsx
+++ src/router.jsx
@@ -1,10 +1,10 @@
 import { createBrowserRouter } from 'react-router-dom';
-import App from './App';
+import Home from './pages/Home';
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <App />,
+    path: '',
+    element: <Home />,
   },
 ]);
 
