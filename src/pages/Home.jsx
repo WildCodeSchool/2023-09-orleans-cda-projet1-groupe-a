e6117ff63384fe54b9b16e8c18d861475d81cf82
greegouze <gregory.silvestri45@gmail.com>
2023-10-06 14:54:36 +0200

    nav-bar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,7 +18,7 @@ function Home() {
 
       {homeVisible && (
         <div className="bg-[url('/BG.avif')] h-screen transform-3d backface-visibility animate-back">
-            <NavBar />
+          <NavBar />
           <div className="flex justify-center">
             <h1 className="text-[#f7f9ef] text-[9rem] drop-shadow-[-1px_1px_4px_rgba(38,41,33,0.97)] font-light line-clamp-1 my-40">
               JAGA STUDIO
