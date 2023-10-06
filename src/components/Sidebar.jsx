8cfa605c94c8498ac5181f7c465eecc4630a11bd
jaalazard <ja.alazard@proton.me>
2023-10-06 15:52:50 +0200

    changes dones

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -12,19 +12,19 @@ const Sidebar = () => {
   return (
     <div
       id="sidebar"
-      className={`flex flex-col w-64 bg-gradient-to-r from-[--dark] opacity-95 via-slate-800-opacity-80 to-slate-500-opacity-30 h-screen pl-8 shadow-2xl transition-all transform ${
+      className={`flex flex-col w-64 bg-gradient-to-r from-dark opacity-95 via-slate-800-opacity-80 to-slate-500-opacity-30 h-screen pl-8 shadow-2xl transition-all transform ${
         isSidebarVisible ? 'translate-x-0' : '-translate-x-full duration-500'
       }`}
     >
       <div className="relative ">
         <div class="absolute right-3 top-3">
-          <button onClick={toggleSidebar} className="text-[--light]">
+          <button onClick={toggleSidebar} className="text-light">
             <X />
           </button>
         </div>
       </div>
 
-      <div className="flex flex-col pt-28 text-light]">
+      <div className="flex flex-col pt-28 text-light">
         <NavLink to="#" className="p-3 text-lg hover:opacity-75">
           HOME
         </NavLink>
