23cfb7a7f4e356f36e307fa109e6244a2ea4c1c0
jaalazard <ja.alazard@proton.me>
2023-10-06 14:54:52 +0200

    just a sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -12,26 +12,26 @@ const Sidebar = () => {
   return (
     <div
       id="sidebar"
-      className={`flex flex-col w-64 bg-gradient-to-r from-slate-900 to-stone-400 bg-opacity-25 h-screen pl-8 shadow-2xl transition-all transform ${
+      className={`flex flex-col w-64 bg-gradient-to-r from-[--dark] opacity-95 via-slate-800-opacity-80 to-slate-500-opacity-30 h-screen pl-8 shadow-2xl transition-all transform ${
         isSidebarVisible ? 'translate-x-0' : '-translate-x-full duration-500'
       }`}
     >
       <div className="relative ">
         <div class="absolute right-3 top-3">
-          <button onClick={toggleSidebar} className="text-white">
+          <button onClick={toggleSidebar} className="text-[--light]">
             <X />
           </button>
         </div>
       </div>
 
-      <div className="flex flex-col pt-28">
-        <NavLink to="#" className="p-3 text-white text-lg hover:opacity-75">
+      <div className="flex flex-col pt-28 text-[--light]">
+        <NavLink to="#" className="p-3 text-lg hover:opacity-75">
           HOME
         </NavLink>
-        <NavLink to="#" className="p-3 text-white text-lg hover:opacity-75">
+        <NavLink to="#" className="p-3 text-lg hover:opacity-75">
           ARTISTS
         </NavLink>
-        <NavLink to="#" className="p-3 text-white text-lg hover:opacity-75">
+        <NavLink to="#" className="p-3 text-lg hover:opacity-75">
           GALLERY
         </NavLink>
       </div>
