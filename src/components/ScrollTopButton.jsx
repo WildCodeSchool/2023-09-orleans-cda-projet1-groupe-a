fd4cb3ff9438246a2f710263097e306b43e0d9d8
Abecker37 <72383676+Abecker37@users.noreply.github.com>
2023-10-13 11:51:40 +0200

    scroll to top button

---
+++ src/components/ScrollTopButton.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { ChevronUp } from 'lucide-react';
+function ScrollTopButton({ relativeTo }) {
+  const [showTopBtn, setShowTopBtn] = useState(false);
+
+  useEffect(() => {
+    relativeTo.addEventListener('scroll', () => {
+      console.log(relativeTo.scrollTop);
+      if (relativeTo.scrollTop > 100) {
+        setShowTopBtn(true);
+      } else {
+        setShowTopBtn(false);
+      }
+    });
+    console.log(relativeTo);
+  }, []);
+
+  const goToTop = () => {
+    relativeTo.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  return (
+    <>
+      {showTopBtn && (
+        <button
+          className="fixed bottom-[50px] right-[50px] h-[50px] w-[50px] cursor-pointer"
+          onClick={goToTop}
+        >
+          <ChevronUp color="#000000" absoluteStrokeWidth />
+        </button>
+      )}
+    </>
+  );
+}
+
+export default ScrollTopButton;
