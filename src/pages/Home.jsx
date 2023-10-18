f97fb7075629067e4654e28d060ed6f5706a38d0
greegouze <gregory.silvestri45@gmail.com>
2023-10-18 14:03:43 +0200

    home zone 1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,54 @@
+import myImage from '../assets/images/image-l.png';
+import myImage2 from '../assets/images/image-r.png';
+import { motion, AnimatePresence } from 'framer-motion';
+
 function Home() {
+  const variants = {
+    offscreen: {
+      y: 300,
+    },
+    onscreen: {
+      y: 3,
+      transition: {
+        type: 'spring',
+        bounce: 0.1,
+        duration: 1,
+      },
+    },
+  };
+
+  const variantPicture = {
+    offscreen: {
+      y: 350,
+      x: 350,
+    },
+    onscreen: {
+      y: 3,
+      x: 0,
+      transition: {
+        type: 'spring',
+        bounce: 0.1,
+        duration: 1.9,
+      },
+    },
+  };
+
+  const variantPicture2 = {
+    offscreen: {
+      y: 350,
+      x: -350,
+    },
+    onscreen: {
+      y: 3,
+      x: 0,
+      transition: {
+        type: 'spring',
+        bounce: 0.1,
+        duration: 1.9,
+      },
+    },
+  };
+
   return (
     <>
       <div className="transform-3d backface-visibility h-screen animate-back bg-[url('/BG.avif')]">
@@ -8,6 +58,36 @@ function Home() {
           </h1>
         </div>
       </div>
+      <AnimatePresence>
+        <motion.div
+          variants={variants}
+          initial="offscreen"
+          whileInView="onscreen"
+          className="my-20 flex flex-col items-center justify-center gap-4"
+        >
+          <h2 className="text-6xl">Welcome to Jaga </h2>
+          <h2 className="text-6xl">Redefining Chicago Art</h2>
+        </motion.div>
+
+        <div className="flex gap-11">
+          <motion.img
+            variants={variantPicture2}
+            initial="offscreen"
+            whileInView="onscreen"
+            className="w-[48%]"
+            src={myImage}
+            alt=""
+          />
+          <motion.img
+            variants={variantPicture}
+            initial="offscreen"
+            whileInView="onscreen"
+            className="w-[48%]"
+            src={myImage2}
+            alt=""
+          />
+        </div>
+      </AnimatePresence>
     </>
   );
 }
