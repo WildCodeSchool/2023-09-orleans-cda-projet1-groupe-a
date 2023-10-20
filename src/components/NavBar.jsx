e44c7d264b9a3bb76da5adcb2da7df625db59fbb
greegouze <gregory.silvestri45@gmail.com>
2023-10-20 11:50:41 +0200

    fixed side bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -12,6 +12,10 @@ export default function NavBar() {
   const [isOpen, setIsOpen] = useState(false);
   const [navVisible, setNavVisible] = useState(false);
 
+  const toggleSidebar = () => {
+    setIsOpen((prev) => !prev);
+  };
+
   const handleMouseEnter = () => {
     setInputOpen(true);
   };
@@ -35,7 +39,7 @@ export default function NavBar() {
       {navVisible && (
         <nav className="via-slate-800-opacity-80 to-slate-500-opacity-30 absolute z-10 flex w-full items-center justify-between bg-gradient-to-b from-dark px-7 py-4 pb-12 opacity-95">
           {/*  menu burger */}
-          <MenuToggle isOpen={isOpen} setIsOpen={setIsOpen} />
+          <MenuToggle isOpen={isOpen} toggleSidebar={toggleSidebar} />
           <div className="group relative flex cursor-pointer justify-center">
             {/*  logo */}
             <Link to={'/'}>
@@ -64,7 +68,7 @@ export default function NavBar() {
               />
             </button>
           </form>
-          <SideBar isOpen={isOpen} />
+          <SideBar isOpen={isOpen} toggleSidebar={toggleSidebar} />
           {ref.current ? <ScrollTopButton relativeTo={ref.current} /> : null}
         </nav>
       )}
