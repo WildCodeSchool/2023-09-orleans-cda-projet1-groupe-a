ba426308e92c9ff278e3c7dd363d6d6fbd8f2a8c
jaalazard <ja.alazard@proton.me>
2023-10-25 16:43:53 +0200

    fixing footer disposition

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,13 +4,15 @@ import justin from '../assets/justin.png';
 import amaury from '../assets/amaury.png';
 export default function Footer() {
   return (
-    <footer className="mt-20 h-full w-full text-center ">
+    <footer className="mt-20 h-full w-full px-32 text-center">
       <div className="flex flex-row items-center gap-8 pt-10">
-        <img src={ja} alt="JA" className=" ml-16 w-48" />
-        <img src={justin} alt="Justin" className=" w-48" />
-        <img src={greg} alt="Greg" className=" w-48" />
-        <img src={amaury} alt="Amaury" className=" mr-80 w-48" />
-        <p className="mb-4 flex items-center text-black">Legal Notice</p>
+        <div className="mx-auto flex">
+          <img src={ja} alt="JA" className="w-48" />
+          <img src={justin} alt="Justin" className=" w-48" />
+          <img src={greg} alt="Greg" className=" w-48" />
+          <img src={amaury} alt="Amaury" className="w-48" />
+        </div>
+        <p className="mb-4 text-black">Legal Notice</p>
       </div>
     </footer>
   );
