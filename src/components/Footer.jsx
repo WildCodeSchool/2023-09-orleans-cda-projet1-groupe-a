353e04fa0fca91d55eb95de235e87fa99f39bf6b
Justine Clément-Savary <clement.savary.justine@gmail.com>
2023-10-30 11:00:52 +0100

    remove <span>

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,21 +7,11 @@ export default function Footer() {
     <>
       <footer className="mx-4 mb-4 mt-28 rounded-lg bg-light shadow-lg">
         <div className="mx-auto flex w-full max-w-screen-xl flex-wrap items-center justify-between p-2">
-          <span>
-            <img src={ja} alt="JA" className="mx-11 w-28" />
-          </span>
-          <span>
-            <img src={justin} alt="Justin" className="mx-11 w-28" />
-          </span>
-          <span>
-            <img src={greg} alt="Greg" className="mx-11 w-28" />
-          </span>
-          <span>
-            <img src={amaury} alt="Amaury" className="mx-11 w-28" />
-          </span>
-          <span className="mt-3">
-            <div className="mx-11 mb-4">Legal notice</div>
-          </span>
+          <img src={ja} alt="JA" className="mx-11 w-28" />
+          <img src={justin} alt="Justin" className="mx-11 w-28" />
+          <img src={greg} alt="Greg" className="mx-11 w-28" />
+          <img src={amaury} alt="Amaury" className="mx-11 w-28" />
+          <div className="mx-11 mb-4">Legal notice</div>
         </div>
       </footer>
     </>
