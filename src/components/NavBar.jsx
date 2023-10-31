6f094611de12071d1cc8512bc5eb65605e627fbe
greegouze <gregory.silvestri45@gmail.com>
2023-10-31 15:14:22 +0100

    hidden nav

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,12 +3,22 @@ import { useEffect, useState, useRef } from 'react';
 import Logo from './icon/Logo';
 import MenuToggle from './MenuToggle';
 import SideBar from './Sidebar';
-import ScrollTopButton from './ScrollTopButton';
 import SearchInput from './SearchInput';
+import { motion, useMotionValueEvent } from 'framer-motion';
 
-export default function NavBar({ scrollRef }) {
+export default function NavBar({ scrollYProgress }) {
   const [isOpen, setIsOpen] = useState(false);
   const [navVisible, setNavVisible] = useState(false);
+  const [hidden, setHidden] = useState(false);
+
+  useMotionValueEvent(scrollYProgress, 'change', (latest) => {
+    const previous = scrollYProgress.getPrevious();
+    if (latest > previous && latest > 0.028) {
+      setHidden(true);
+    } else {
+      setHidden(false);
+    }
+  });
 
   const buttonRef = useRef(null);
 
@@ -26,7 +36,12 @@ export default function NavBar({ scrollRef }) {
   return (
     <>
       {navVisible && (
-        <nav className="via-slate-800-opacity-80 to-slate-500-opacity-30 absolute z-10 flex w-full items-center justify-between bg-gradient-to-b from-dark px-7 py-4 pb-12 opacity-95">
+        <motion.nav
+          variants={{ visible: { y: 0 }, hidden: { y: '-100%' } }}
+          animate={hidden ? 'hidden' : 'visible'}
+          transition={{ duration: 0.55, ease: 'easeInOut' }}
+          className="via-slate-800-opacity-80 to-slate-500-opacity-30 absolute z-10 flex w-full items-center justify-between bg-gradient-to-b from-dark px-7 py-4 pb-12 opacity-95"
+        >
           {/*  menu burger */}
           <div className="w-1/3">
             <MenuToggle
@@ -49,10 +64,7 @@ export default function NavBar({ scrollRef }) {
             setIsOpen={setIsOpen}
             buttonRef={buttonRef}
           />
-          {scrollRef.current ? (
-            <ScrollTopButton relativeTo={scrollRef.current} />
-          ) : null}
-        </nav>
+        </motion.nav>
       )}
     </>
   );
