7d5d088ba9596f3130a140ea93245ca1eddcb1e5
YDrogen <ydrogen45@gmail.com>
2023-11-03 15:08:54 +0100

    ci: rerun

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import router from './router';
-
-import './main.css';
 import { RouterProvider } from 'react-router-dom';
+
+import router from './router';
+import './main.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
