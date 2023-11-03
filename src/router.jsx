10b3822c595a59fc2e81aa61e666f0da458c4b50
greegouze <gregory.silvestri45@gmail.com>
2023-11-03 15:13:38 +0100

    router

--- src/router.jsx
+++ src/router.jsx
@@ -8,43 +8,45 @@ import Layout from './components/Layout';
 import LegalNotice from './pages/LegalNotice';
 import Artworks from './pages/Artworks';
 
-const router = createBrowserRouter([
-  {
-    element: <Layout />,
-    children: [
-      {
-        path: '',
-        element: <Home />,
-      },
-      {
-        path: '/all-artists',
-        element: <AllArtists />,
-      },
-      {
-        path: '/gallery',
-        element: <Gallery />,
-      },
-      {
-        path: '/artists/:artist_id',
-        element: <Artists />,
-      },
-      {
-        path: '/legal-notice',
-        element: <LegalNotice />,
-      },
-      {
-        path: '/artworks/:artwork_id',
-        element: <Artworks />,
-      },
-    ],
-  },
-  {
-    path: '/404',
-    element: <NotFound />,
-  },
+const router = createBrowserRouter(
+  [
+    {
+      element: <Layout />,
+      children: [
+        {
+          path: '',
+          element: <Home />,
+        },
+        {
+          path: '/all-artists',
+          element: <AllArtists />,
+        },
+        {
+          path: '/gallery',
+          element: <Gallery />,
+        },
+        {
+          path: '/artists/:artist_id',
+          element: <Artists />,
+        },
+        {
+          path: '/legal-notice',
+          element: <LegalNotice />,
+        },
+        {
+          path: '/artworks/:artwork_id',
+          element: <Artworks />,
+        },
+      ],
+    },
+    {
+      path: '/404',
+      element: <NotFound />,
+    },
+  ],
   {
     basename: import.meta.env.BASE_URL,
   },
-]);
+);
 
 export default router;
